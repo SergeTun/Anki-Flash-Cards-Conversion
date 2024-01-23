be59a5056d38e414e490ac820e0ae9dc62f04f9e
Serge Thurain Bolduc Tun <sergethurain2@gmail.com>
2024-01-22 21:58:31 -0500

    declared functions

---
+++ 1.py
@@ -0,0 +1,25 @@
+import genanki
+import argparse
+import sqlite3
+
+notes = []
+frontCard = []
+
+def getCards():
+    pass
+
+def getNotes():
+    pass
+
+def noteFormat(notes):
+    for char in notes:
+        if notes.isupper():
+            notes = frontCard
+        else:
+            
+            
+def createFrontCard():
+    pass
+
+def createBackCard():
+    pass 
