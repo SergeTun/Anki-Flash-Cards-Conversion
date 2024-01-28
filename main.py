ec357d1112cc0699d3c5001704c65e57b6427c84
Serge Thurain Bolduc Tun <98868650+SergeTun@users.noreply.github.com>
2024-01-27 21:10:37 -0500

    Abandoned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import genanki
 import os
-import io
 import string
 import re
 import random
@@ -23,18 +22,23 @@ def noteFormat():
     return note_format
     )   
 
-def createNotes(note_format):
+def createNotes(note_format, file_contents):
     my_note = genanki.note(
         model = note_format,
-        fields=[]
+        fields=[file_contents]
     )
 
 def process_folder(folder_path):
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
     for file_name in files:
-        if file_name.endswith('txt'):
-            pass
+        if file_name.endswith('.txt'):
+            file_path = os.path.join(folder_path, file_name)
+        
+            with open(file_path, 'r', encoding='utf-8') as file:
+                file_contents = file.read
+
+
 
 def main():
     userName = os.getlogin()
